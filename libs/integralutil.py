bc61318efdd589fe6e7f2b31c6a3fc35c0ffefcc
Toliz <apost_panag@yahoo.gr>
2017-05-11 20:46:36 +0300

    Improved API

--- libs/integralutil.py
+++ libs/integralutil.py
@@ -3,7 +3,7 @@ from sympy.solvers import solve
 from math import inf
 
 
-def get_interval_points(n, func, interval):
+def getIntervalPoints(n, func, interval):
     """
     Method to separate an interval to equal intervals and get the value of a
     function of their leftmost points
@@ -14,15 +14,17 @@ def get_interval_points(n, func, interval):
     func:
         the function we are interested in. Func is an instance of fuction class 
         that is implemented in functiontools.py
+
     interval:
         interval is a list, i.e. [2,5.4] that specifies the interval in the x axis 
         that will be separated in n equal integrals
 
-    ex. if we have the interval [1,3] and n = 2 then xs = [1,2,3]
+    ex. if we have the interval [1, 3] and n = 2 then xs = [1, 2, 3]
     and if func = 2x + 1, fs = [3, 5, 7]
     """
     if len(interval) != 2:
         raise ValueError("interval must have only two elements, i.e. [3, 3.5]")
+
     a, b = interval[:]
     if(b < a):
         a, b = b, a
@@ -31,10 +33,8 @@ def get_interval_points(n, func, interval):
     
     dx = (b - a)/float(n)
 
-    # xs contains the points on which we separate the interval
-    # and fs the value of f at these points
-    xs = [a + i * dx for i in range(n + 1)]
-    fs = [func(x) for x in xs]
+    # contains the values of the points on the interval
+    fs = [func(a + i * dx) for i in range(n+1)]
 
     return dx, fs
 
@@ -91,3 +91,4 @@ if __name__ == "__main__":
     f = function(expr)
     
     print(funcMax(f, [-10, 10]))
+    print(getIntervalPoints(10, function('x'), [0, 1]))
